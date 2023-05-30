05726d88b2193d7ce6005cbc12f0090c27fa82a8
PiroAvni <avnipiro.dev@gmail.com>
2023-05-30 14:26:46 +0100

    add dashbroad and navigate function

--- educational_app/src/components/PrivateRoute/index.jsx
+++ educational_app/src/components/PrivateRoute/index.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { DashBroad } from '../../screens';
+
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
